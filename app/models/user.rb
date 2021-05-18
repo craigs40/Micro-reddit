b744c85bdc75fbeaaf9d34da04244b39c563fe7c
craigs40 <craig.shay40@gmail.com>
2021-05-18 16:49:03 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates (:username, presence: true, uniqueness: true, length: { minimum: 3 })
+  validates :username, length: { minimum: 3 }
 
   has_many :posts
   has_many :comments
